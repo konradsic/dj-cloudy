6a20f380ad02b6ee2d398552498781eb2256dd7a
konradsic <sicinskikonrad@gmail.com>
2022-10-22 22:00:38 +0200

    Add requirements.txt and fix description of /playlisys add-song being too long

--- cogs/playlist-adapter.py
+++ cogs/playlist-adapter.py
@@ -26,7 +26,7 @@ class PlaylistGroupCog(commands.GroupCog, name="playlists"):
     async def playlist_create_command(self, interation: discord.Interaction, name: str):
         pass
 
-    @app_commands.command(name="add-song", description="Add a song to your playlist. Use starred in the name of playlist to add the song yo your starred-songs playlist")
+    @app_commands.command(name="add-song", description="Add a song to your playlist. Use 'starred' when you want to add it to your starred songs playlist")
     @app_commands.describe(name_or_id="Name of the playlist you want to add the song to")
     @app_commands.describe(song="Name of the song you want to add")
     async def playlist_add_song_command(self, interation: discord.Interaction, name_or_id: str, song: str):
@@ -53,10 +53,10 @@ async def setup(bot):
     help_utils.register_command("playlists view", "View your or user's playlists", "Music: Playlist management", [("user", "View this user's playlists", False)])
     help_utils.register_command("playlists create", "Create a new playlist", "Music: Playlist management", [("name", "Name of the playlist", True)])
     help_utils.register_command("playlists add-song", 
-        "Add a song to your playlist. Use starred in the name of playlist to add the song yo your starred-songs playlist", "Music: Playlist management", 
+        "Add a song to your playlist. Use 'starred' when you want to add it to your starred songs playlist", "Music: Playlist management", 
         [("name_or_id", "Name of the playlist you want to add the song to", True),("song", "Name of the song you want to add",True)])
     help_utils.register_command("playlists remove-song", "Remove a song from your playlist", "Music: Playlist management", 
-        [("name_or_id", "Name of the playlist you want to remove the song from", True),("index", "Index of the song you want to remove (1-playlist len)")])
+        [("name_or_id", "Name of the playlist you want to remove the song from", True),("index", "Index of the song you want to remove (1-playlist len)", True)])
     help_utils.register_command("playlists play", "Play your playlist!", "Music: Playlist management", 
         [("name_or_id", "Name of the playlist you want to play", True), ("replace_queue", "Wherever to replace the queue with the playlist or just append", False)])
     help_utils.register_command("playlists remove-playlist", "Remove a playlist", "Music: Playlist management", [("name_or_id", "Name of the playlist you want to remove", True)])
