8b6fa333c8e50db3f5d8a83ded35c408d2c4bb6e
Konrad <sicinskikonrad@gmail.com>
2024-02-04 19:20:20 +0100

    Fix critical issue with custom embeds

--- lib/ui/embeds.py
+++ lib/ui/embeds.py
@@ -15,7 +15,7 @@ class ShortEmbed(discord.Embed): # used only with description
         super().__init__(color=color, description=description, **kwargs)
         
 class NormalEmbed(discord.Embed): # normal embed, used everywhere else
-    def __init__(self, timestamp: bool, footer_add: str, replace_footer: bool, footer: FooterType=FooterType.MADE_BY, color=BASE_COLOR, **kwargs):
+    def __init__(self, timestamp: bool=False, footer_add: str="", replace_footer: bool=False, footer: FooterType=FooterType.MADE_BY, color=BASE_COLOR, **kwargs):
         super().__init__(color=color, **kwargs)
         footer = footer.value + footer_add
         if replace_footer: footer = footer_add
